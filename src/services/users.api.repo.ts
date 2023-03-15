e88c7511399b15a2f38c59b3be3a6ca86804cccb
Daniel <dursuaf@gmail.com>
2023-03-15 13:15:40 +0100

    Add final extra line

---
+++ src/services/users.api.repo.ts
@@ -0,0 +1,28 @@
+import { ProtoUser, User } from "../models/user.model";
+import { LoginData } from "../reducer/user.slice";
+
+export interface UserRepoStructure {
+  registerUser(user: ProtoUser): Promise<LoginData>;
+  loginUser(user: ProtoUser): Promise<LoginData>;
+}
+
+export class UserApiRepo {
+  url: string;
+  constructor() {
+    this.url = "http://localhost:4500/users";
+  }
+  async loginUser(user: ProtoUser): Promise<LoginData> {
+    const resp = await fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    if (!resp.ok)
+      throw new Error("Error Http: " + resp.status + ". " + resp.statusText);
+    const data = (await resp.json()) as LoginData;
+
+    return data;
+  }
+}
