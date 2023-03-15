9deae940e6ffb9e296bcabafb54b95b7a0262869
Daniel <dursuaf@gmail.com>
2023-03-15 19:11:54 +0100

    Add register repo method

--- src/services/users.api.repo.test.ts
+++ src/services/users.api.repo.test.ts
@@ -1,21 +1,35 @@
-import { ProtoUser } from "../models/user.model";
+import { protoUser } from "../models/user.model";
 import { LoginData } from "../reducer/user.slice";
-import { UserApiRepo } from "./users.api.repo";
+import { UsersApiRepo } from "./users.api.repo";
 
-const fetch = jest.fn();
 const mockResp = {
   token: "test",
   user: "test",
 } as unknown as LoginData;
 const mockUser = {
   name: "test",
-} as ProtoUser;
+} as protoUser;
 describe("Given the users api repo", () => {
-  const repo = new UserApiRepo();
+  const repo = new UsersApiRepo();
 
   describe("When instantiated", () => {
     test("Then it should make a new instance", () => {
-      expect(repo).toBeInstanceOf(UserApiRepo);
+      expect(repo).toBeInstanceOf(UsersApiRepo);
+    });
+  });
+  describe("When we call the register method", () => {
+    test("if the answer is not ok, then it should throw an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("error");
+      const result = repo.registerUser(mockUser);
+      await expect(result).rejects.toThrow();
+    });
+    test("If the answer is ok, then it should return the login info", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue(mockResp),
+      });
+      const result = await repo.registerUser(mockUser);
+      expect(result).toEqual(mockResp);
     });
   });
   describe("When we call the login user method", () => {
--- src/services/users.api.repo.ts
+++ src/services/users.api.repo.ts
@@ -1,17 +1,31 @@
-import { ProtoUser } from "../models/user.model";
+import { User } from "../models/user.model";
 import { LoginData } from "../reducer/user.slice";
 
 export interface UserRepoStructure {
-  registerUser(user: ProtoUser): Promise<LoginData>;
-  loginUser(user: ProtoUser): Promise<LoginData>;
+  registerUser(user: Partial<User>): Promise<User>;
+  loginUser(user: Partial<User>): Promise<LoginData>;
 }
 
-export class UserApiRepo {
+export class UsersApiRepo {
   url: string;
   constructor() {
     this.url = "http://localhost:4500/users/";
   }
-  async loginUser(user: ProtoUser): Promise<LoginData> {
+  async registerUser(user: Partial<User>): Promise<User> {
+    const resp = await fetch(this.url + "register", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    if (!resp.ok)
+      throw new Error("Error Http: " + resp.status + ". " + resp.statusText);
+    const data = (await resp.json()) as User;
+
+    return data;
+  }
+  async loginUser(user: Partial<User>): Promise<LoginData> {
     const resp = await fetch(this.url + "login", {
       method: "POST",
       body: JSON.stringify(user),
