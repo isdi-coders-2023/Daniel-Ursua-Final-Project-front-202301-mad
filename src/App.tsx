d676863b54fa08fdeaace1eb3f57c8d420d3652f
Daniel <dursuaf@gmail.com>
2023-03-16 20:23:46 +0100

    Fix index problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { LogUserForm } from "./components/forms/user.forms";
 
 function App() {
-  return <h1>Working on it</h1>;
+  return (
+    <>
+      <h1>Working on it</h1>
+      <LogUserForm></LogUserForm>
+    </>
+  );
 }
 
 export default App;
