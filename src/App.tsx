61816bd0d2c9fee43864bf0e37b77fb9606cd394
Daniel <dursuaf@gmail.com>
2023-03-17 10:02:06 +0100

    Update app test

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { LogUserForm } from "./components/forms/user.forms";
 function App() {
   return (
     <>
-      <h1>Working on it</h1>
-      <LogUserForm></LogUserForm>
+      <h1>PlantApp</h1>
     </>
   );
 }
