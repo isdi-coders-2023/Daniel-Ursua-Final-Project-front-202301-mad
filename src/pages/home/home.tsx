949cc42a15dff5ad6239ec752c4c151d640d49d6
Daniel <dursuaf@gmail.com>
2023-03-17 15:27:44 +0100

    Add home, login and register

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+  return (
+    <>
+      <button>
+        <a href="../pages/login.tsx">Log in</a>
+      </button>
+      <button>
+        <a href="../pages/register.tsx">Register</a>
+      </button>
+    </>
+  );
+};
