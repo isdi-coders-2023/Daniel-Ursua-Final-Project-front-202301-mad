bdca65550980aab81e980145171181ab02b6be4e
Daniel <dursuaf@gmail.com>
2023-03-17 16:32:50 +0100

    Add router to test

--- src/pages/register/register.test.tsx
+++ src/pages/register/register.test.tsx
@@ -3,13 +3,16 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../app/store";
 import RegisterPage from "./register";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Given the register page", () => {
   describe("When it is render", () => {
     beforeEach(() => {
       render(
         <Provider store={store}>
-          <RegisterPage />
+          <Router>
+            <RegisterPage />
+          </Router>
         </Provider>
       );
     });
