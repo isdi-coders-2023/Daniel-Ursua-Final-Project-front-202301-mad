49fc468e14689f3fbe14ecc906a9f72325af7d5b
Daniel <dursuaf@gmail.com>
2023-03-17 18:55:48 +0100

    Update home test

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -1,18 +1,24 @@
-import { render, screen } from "@testing-library/react";
+/* eslint-disable testing-library/no-render-in-setup */
+import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./home";
-import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter as Router } from "react-router-dom";
 
 describe("Given the home page", () => {
   describe("When it is render", () => {
-    render(
-      <Router>
-        <Home></Home>
-      </Router>
-    );
-    test("Then it should return the login and register button", () => {
-      const element = screen.getAllByRole("button");
-      expect(element[0]).toBeInTheDocument();
-      expect(element[1]).toBeInTheDocument();
+    beforeEach(() => {
+      render(
+        <Router>
+          <Home></Home>
+        </Router>
+      );
+    });
+    describe("When the user click on login button", () => {
+      test("Then, handlelogin should go to login", async () => {
+        const button = screen.getAllByRole("button");
+        fireEvent.click(button[0]);
+        const element = await screen.findAllByRole("heading");
+        expect(element.length).toBe(1);
+      });
     });
   });
 });
