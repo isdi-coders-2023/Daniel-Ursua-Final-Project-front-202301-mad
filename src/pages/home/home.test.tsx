4054fb69fc628892a8535bdf1cbe34ff27f7d76f
Daniel <dursuaf@gmail.com>
2023-03-17 16:37:12 +0100

    Add router to tests

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Home from "./home";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Given the home page", () => {
   describe("When it is render", () => {
     render(
-      <>
+      <Router>
         <Home></Home>
-      </>
+      </Router>
     );
     test("Then it should return the login and register button", () => {
       const element = screen.getAllByRole("button");
