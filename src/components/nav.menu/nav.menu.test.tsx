16a8a1df3aaa9a110c3ae8adf4f06520a9aa000f
Daniel <dursuaf@gmail.com>
2023-03-22 16:02:04 +0100

    Update nav test

--- src/components/nav.menu/nav.menu.test.tsx
+++ src/components/nav.menu/nav.menu.test.tsx
@@ -1,28 +1,37 @@
-import { render } from "@testing-library/react";
+/* eslint-disable testing-library/no-render-in-setup */
+import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../app/store";
 import { MenuOption } from "../header/header";
-import { NavMenu } from "./nav.menu";
-
-const mockOptions: MenuOption[] = [
-  {
-    label: "test",
-    path: "test",
-  },
-  {
-    label: "test2",
-    path: "test2",
-  },
-];
+import { MenuProps, NavMenu } from "./nav.menu";
+import { MemoryRouter as Router } from "react-router-dom";
 
 describe("Given the nav component", () => {
-  render(
-    <Provider store={store}>
-      <NavMenu menuOptions={mockOptions}></NavMenu>
-    </Provider>
-  );
+  const mockOptions: MenuOption[] = [
+    {
+      label: "test",
+      path: "test",
+    },
+    {
+      label: "test2",
+      path: "test2",
+    },
+  ];
 
+  const mockProp: MenuProps = {
+    options: mockOptions,
+  };
   describe("When it is render", () => {
-    test("Then it should print an unorder list", () => {});
+    test("Then it should print an unorder list", () => {
+      render(
+        <Provider store={store}>
+          <Router>
+            <NavMenu options={mockProp}></NavMenu>
+          </Router>
+        </Provider>
+      );
+      const element = screen.getByDisplayValue(/test/i);
+      expect(element).toBeInTheDocument();
+    });
   });
 });
