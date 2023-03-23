1d6432f330efe54558c71d841e59c83aff0bcf25
Daniel <dursuaf@gmail.com>
2023-03-23 16:38:26 +0100

    Add card component

---
+++ src/components/card/card.plant.test.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable testing-library/no-render-in-setup */
+import { fireEvent, render, screen } from "@testing-library/react";
+import { usePlants } from "../../hook/use.plants";
+import { PlantInTheList, Ubication } from "../../models/plant.model";
+import { PlantsApiRepo } from "../../services/plants.api.repo";
+import CardPlant from "./card.plant";
+
+jest.mock("../../services/plants.api.repo.ts");
+jest.mock("../../hook/use.plants");
+
+const mockPlant: PlantInTheList = {
+  photo: "test image",
+  ubication: "test ubication" as Ubication,
+  name: "test name",
+  id: "test id",
+};
+const mockUpdatePlant = jest.fn;
+beforeEach(() => {
+  (usePlants as jest.Mock).mockReturnValue({ updatePlant: jest.fn() });
+  render(<CardPlant info={mockPlant}></CardPlant>);
+});
+
+describe("Given the card plant component", () => {
+  describe("When it is render", () => {
+    test("Then it should print differents elements", async () => {
+      const image = await screen.findByText(/image/i);
+      expect(image).toBeInTheDocument();
+      const ubication = await screen.findByText(/ubication/i);
+      expect(ubication).toBeInTheDocument();
+      const name = await screen.findByText(/name/i);
+      expect(name).toBeInTheDocument();
+    });
+  });
+  describe("When we click on the component", () => {
+    test("It should call the updatePlant method", async () => {
+      const image = await screen.findByText(/image/i);
+      await fireEvent.click(image);
+      expect(usePlants({} as PlantsApiRepo).updatePlant).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/components/card/card.plant.tsx
@@ -0,0 +1,33 @@
+import { SyntheticEvent, useMemo } from "react";
+import { usePlants } from "../../hook/use.plants";
+import { PlantInTheList } from "../../models/plant.model";
+import { PlantsApiRepo } from "../../services/plants.api.repo";
+
+export type CardProps = {
+  info: PlantInTheList;
+};
+
+export default function CardPlant({ info }: CardProps) {
+  const repo = useMemo(() => new PlantsApiRepo(), []);
+  const { updatePlant } = usePlants(repo);
+
+  const handleClick = async (ev: SyntheticEvent) => {
+    await updatePlant(info.id);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <span>{info.photo}</span>
+      <p>{info.ubication}</p>
+      <p>{info.name}</p>
+    </div>
+  );
+}
+
+//Código para aprovecharlo en la tarjeta detalle
+// const repo = useMemo (() => new UsersApiRepo(), [])
+// const { users } = useUsers(repo)
+// const actualUser = users.users.userLogged?
+// const handleClickMyPlants = (ev: SyntheticEvent) => {
+//   actualUser.plantList.push()
+// };
