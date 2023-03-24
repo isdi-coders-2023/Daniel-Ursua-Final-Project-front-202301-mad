74b1159e1483f24ae1f2f1b96855aeb4f77746b3
Daniel <dursuaf@gmail.com>
2023-03-24 11:39:43 +0100

    Add .env and update files

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrSPAefmgQnxKukkCkVL8kFnMsLrkl_fA",
-  authDomain: "finalproyect-17862.firebaseapp.com",
-  projectId: "finalproyect-17862",
-  storageBucket: "finalproyect-17862.appspot.com",
-  messagingSenderId: "257326299344",
-  appId: "1:257326299344:web:c4b119ac489a6bee3386d5",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
