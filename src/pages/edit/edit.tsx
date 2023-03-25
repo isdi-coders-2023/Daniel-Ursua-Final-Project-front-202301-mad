997367c13365b88555bac08731580682bcdeeaf7
Daniel <dursuaf@gmail.com>
2023-03-25 17:22:17 +0100

    Add edit page

---
+++ src/pages/edit/edit.tsx
@@ -0,0 +1,11 @@
+import { PlantForm } from "../../components/forms/plant.form";
+
+export default function edit() {
+  return (
+    <>
+      <h1>Edit</h1>
+      <h2>your plant</h2>
+      <PlantForm></PlantForm>
+    </>
+  );
+}
