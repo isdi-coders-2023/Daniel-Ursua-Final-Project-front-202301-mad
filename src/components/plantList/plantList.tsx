17b5451402beb1e05afcdf76729ec06e0ff30608
Daniel <dursuaf@gmail.com>
2023-03-26 21:14:13 +0200

    Fix plant list code

--- src/components/plantList/plantList.tsx
+++ src/components/plantList/plantList.tsx
@@ -1,26 +1,24 @@
 import { useEffect, useMemo } from "react";
-import { Card } from "react-bootstrap";
 import { usePlants } from "../../hook/use.plants";
 import { PlantInTheList } from "../../models/plant.model";
 import { PlantsApiRepo } from "../../services/plants.api.repo";
+import CardPlant from "../card/card.plant";
 
 export default function PlantList() {
-  debugger;
   const repo = useMemo(() => new PlantsApiRepo(), []);
   const { getPlants, plants } = usePlants(repo);
   useEffect(() => {
     getPlants();
-  });
-
+  }, [getPlants]);
   const plantsArray = plants.plantList;
   return (
     <>
       <h1>Let's find your plants!</h1>
-      <main>
+      <ul>
         {plantsArray.map((item: PlantInTheList) => (
-          <Card {...item} key={item.id}></Card>
+          <CardPlant info={item} key={item.id}></CardPlant>
         ))}
-      </main>
+      </ul>
     </>
   );
 }
