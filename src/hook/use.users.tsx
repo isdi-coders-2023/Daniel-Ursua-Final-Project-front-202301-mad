e0f7a751c708538ba08f23523e3fdf08a67631e8
Daniel <dursuaf@gmail.com>
2023-03-28 09:09:52 +0200

    Add check user comprobation

--- src/hook/use.users.tsx
+++ src/hook/use.users.tsx
@@ -23,10 +23,23 @@ export function useUsers(repo: UsersApiRepo) {
       dispatch(setError((error as Error).message));
     }
   };
+  const checkUser = (id: string): Boolean => {
+    try {
+      const actualUser = users.userLogged?.user.id;
+      if (id !== actualUser) {
+        throw new Error("Unauthorized");
+      }
+      return true;
+    } catch (error) {
+      dispatch(setError((error as Error).message));
+      return false;
+    }
+  };
 
   return {
     users,
     loginUser,
     register,
+    checkUser,
   };
 }
