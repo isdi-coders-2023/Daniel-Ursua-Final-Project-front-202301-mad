b5df1a053986c1065d75e27a811a2ca5b67e44aa
Daniel <dursuaf@gmail.com>
2023-03-29 01:16:45 +0200

    Update plantList style

--- src/components/plantList/plantList.tsx
+++ src/components/plantList/plantList.tsx
@@ -3,6 +3,7 @@ import { usePlants } from "../../hook/use.plants";
 import { PlantInTheList } from "../../models/plant.model";
 import { PlantsApiRepo } from "../../services/plants.api.repo";
 import CardPlant from "../card/card.plant";
+import styles from "./plantList.module.scss";
 
 export default function PlantList() {
   const repo = useMemo(() => new PlantsApiRepo(), []);
@@ -13,8 +14,8 @@ export default function PlantList() {
   const plantsArray = plants.plantList;
   return (
     <>
-      <h1>Let's find your plants!</h1>
-      <ul>
+      <h1 className={styles.h1}>Let's find your plants!</h1>
+      <ul className={styles.ul}>
         {plantsArray.map((item: PlantInTheList) => (
           <CardPlant info={item}></CardPlant>
         ))}
