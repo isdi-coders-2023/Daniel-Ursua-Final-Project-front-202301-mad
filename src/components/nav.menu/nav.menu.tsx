59a0fc8694807c1508ed3c43c0d7a16abe7bdc5e
Daniel <dursuaf@gmail.com>
2023-03-29 02:12:11 +0200

    Add navmenu css

--- src/components/nav.menu/nav.menu.tsx
+++ src/components/nav.menu/nav.menu.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { MenuOption } from "../header/header.js";
+import styles from "./nav.menu.module.scss";
 
 export type MenuProps = {
   options: MenuOption[];
@@ -9,9 +10,9 @@ export function NavMenu({ options }: MenuProps) {
   console.log({ options });
   return (
     <nav>
-      <ul>
+      <ul className={styles.ul}>
         {options.map((item: MenuOption) => (
-          <li key={item.label}>
+          <li key={item.label} className={styles.item}>
             <Link to={item.path}>{item.label}</Link>
           </li>
         ))}
